27cfdcff8aa28499c8986d03e624dad486d77a23
Janis Krasemann <janis.krasemann@enpit.de>
2017-12-19 06:38:14 +0100

    Add bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from './profile-pic.jpg'
+import profilePic from './profile-pic.png'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Janis Krasemann`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,10 +27,10 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
+          I am Janis Krasemann from 🇩🇪 and this is kind of my personal homepage.
+          I build stuff using JavaScript 💻 and study computer science at the university of Paderborn 🤓{' '}
+          <a href="https://twitter.com/JanisKra">
+            Visit me on Twitter!
           </a>
         </p>
       </div>
